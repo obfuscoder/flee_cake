430004c44d1ba984899dedf88bf0056142c3cfaf
Kai Lehmann <kai@obfusco.de>
2013-09-29 22:01:20 +0200

    configured initial app with db and salts, added localized and debug_kit plugins

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'V2d0kijxStO3w6xaAm73GoUG1hSMCDNGttg15CwukaXDBzIaWpKXoGtiX6njomz6');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '598391485038656422199915352281');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).

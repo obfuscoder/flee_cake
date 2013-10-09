de85a82fd1cb52fc4c5135f115e675c73a100625
Kai Lehmann <kai@obfusco.de>
2013-10-09 21:28:19 +0200

    uniques validation for emails and other validations and messages

--- app/Model/Item.php
+++ app/Model/Item.php
@@ -2,6 +2,11 @@
 
 class Item extends AppModel {
 	public $belongsTo = array("Seller", "Category");
+
+	public $validate = array(
+		"description" => array("rule" => array("between", "4", "20"), "message" => "Bitte geben Sie eine sinnvolle Beschreibung mit 4 bis 30 Buchstaben ein."),
+		"price" => array("rule" => "notEmpty", "message" => "Bitte geben Sie einen Preis ein.")
+	);
 }
 
 ?>
--- app/Model/Seller.php
+++ app/Model/Seller.php
@@ -4,16 +4,16 @@ class Seller extends AppModel {
 	public $hasMany = "Item";
 
 	public $validate = array(
-		"first_name" => "notEmpty",
-		"last_name" => "notEmpty",
-		"street" => "notEmpty",
-		"zip_code" => array(
-			"rule" => array("between", 5, 5),
-			"message" => "postal code may only consist of 5 digits"
-		),
-		"city" => "notEmpty",
-		"phone" => "numeric",
-		"email" => "email"
+		"first_name" => array("rule" => "notEmpty", "message" => "Bitte geben Sie einen Vornamen ein."),
+		"last_name" => array("rule" => "notEmpty", "message" => "Bitte geben Sie einen Nachnamen ein."),
+		"street" => array("rule" => "notEmpty", "message" => "Bitte geben Sie eine Straße ein."),
+		"zip_code" => array("rule" => array("postal", null, "de"), "message" => "Die Postleitzahl darf nur aus 5 Ziffern bestehen."),
+		"city" => array("rule" => "notEmpty", "message" => "Bitte geben Sie einen Ort ein."),
+		"phone" => array("rule" => array("phone", '/[0-9\/. \-]*/', "de"), "message" => "Bitte geben Sie eine gültige Telefonnummer ein."),
+		"email" => array(
+			"email" => array("rule" => "email", "message" => "Geben Sie bitte eine gültige eMail-Adresse an."),
+			"unique" => array("rule" => "isUnique", "message" => "Es existiert bereits eine Registrierung für diese eMail-Adresse")
+		)
 	);
 }
 

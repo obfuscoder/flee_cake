a7c4b7787a703164e1e6a224fb85ee171afaa0c3
Kai Lehmann <kai@obfusco.de>
2013-10-19 16:12:03 +0200

    fixed item edit bugs

--- app/Controller/ItemsController.php
+++ app/Controller/ItemsController.php
@@ -18,26 +18,27 @@ class ItemsController extends AppController {
 				return $this->redirect(array("action" => "index", $this->request->data["Item"]["seller_id"]));
 			}
 			$this->Session->setFlash(__("Unable to create item"));
+		} else {
+			$item = $this->Item->create();
+			$item["Item"]["seller_id"] = $sellerId;
+			$this->request->data = $item;
 		}
-		$item = $this->Item->create();
 		$this->set("categories", $this->Item->Category->find("list", array("order" => "name")));
-		$item["Item"]["seller_id"] = $sellerId;
-		$this->request->data = $item;
 		$this->render("edit");
 	}
 
 	public function update($id) {
 		if ($this->request->isPut() || $this->request->isPost()) {
-			debug($this->request->data);
 			if ($this->Item->save($this->request->data)) {
 				$this->Session->setFlash(__("Item has been updated."), "default", array('class' => 'success'));
 				return $this->redirect(array("action" => "index", $this->request->data["Item"]["seller_id"]));
 			}
 			$this->Session->setFlash(__("Unable to update item"));
+		} else {
+			$item = $this->Item->findById($id);
+			$this->request->data = $item;
 		}
-		$item = $this->Item->findById($id);
 		$this->set("categories", $this->Item->Category->find("list", array("order" => "name")));
-		$this->request->data = $item;
 		$this->render("edit");
 	}
 
@@ -46,7 +47,6 @@ class ItemsController extends AppController {
 			throw new MethodNotAllowedException();
 		}
 		$item = $this->Item->findById($id);
-		debug($item);
 		if ($this->Item->delete($id)) {
 			$this->Session->setFlash(__("The item has been deleted."), "default", array('class' => 'success'));
 			return $this->redirect(array("action" => "index", $item["Item"]["seller_id"]));
@@ -54,8 +54,6 @@ class ItemsController extends AppController {
 	}
 
 	public function pdf($sellerId) {
-        //Configure::write('debug',0); // Otherwise we cannot use this method while developing
-
         $items = $this->Item->findAllBySellerId($sellerId);
         $this->set("items", $items);
         

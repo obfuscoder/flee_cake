31ceaf6c624d235895f8d6b19968c40138e9bfad
Kai Lehmann <kai@obfusco.de>
2013-10-30 23:35:39 +0100

    used login as username

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -2,7 +2,7 @@
 
 class AdminController extends AppController {
 	public $components = array('Session', 'RequestHandler', "Auth" => array(
-		"authenticate" => array("Digest" => array('fields' => array('username' => 'email'), "passwordHasher" => array()))
+		"authenticate" => array("Digest" => array('fields' => array('username' => 'login')))
 	));
 
 	public function admin_index() {
